5da3cba97d1bfaa0573639271f985342d49a36c1
Katlego-Jikolo <eduv4964861@vossie.net>
2026-06-28 20:04:01 +0200

    Add files via upload

---
+++ buyer_navbar.php
@@ -0,0 +1,45 @@
+
+<nav class= "navbar navbar-expand-lg navbar-dark bg-dark">
+
+
+    <div class= "container">
+
+
+        <a class= "navbar-brand">
+
+            TradeSphere Africa
+
+        </a>
+
+        <div>
+
+            <a class= "btn btn-outline-light me-2" href= "buyer_products.php">
+
+                Products
+
+           </a>
+
+           <a class= "btn btn-outline-light me-2" href= "cart.php">
+
+               Cart
+
+           </a>
+
+           <a class= "btn btn-outline-light me-2" href= "buyer_orders.php">
+
+               Orders
+
+           </a>
+
+           <a class= "btn btn-danger me-2" href= "logout_system.php">
+
+               Logout
+
+           </a>
+
+        </div>
+
+    </div>        
+
+</nav>
+ 
