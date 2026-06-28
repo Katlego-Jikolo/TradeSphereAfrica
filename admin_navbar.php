126a43a7010bf2ed9ad47bcf04021e49f29aff88
Katlego-Jikolo <eduv4964861@vossie.net>
2026-06-28 19:56:30 +0200

    Add files via upload

---
+++ admin_navbar.php
@@ -0,0 +1,51 @@
+<nav class= "navbar navbar-dark bg-dark">
+
+
+    <div class= "container">
+
+
+        <a class= "navbar-brand">
+
+            Admin DashBoard
+
+        </a>
+
+        <div>
+
+            <a class= "btn btn-outline-light me-2" href= "dashboard.php">
+
+                Dashboard
+
+           </a>
+
+           <a class= "btn btn-outline-light me-2" href= "manage_users.php">
+
+               Users
+
+           </a>
+
+           <a class= "btn btn-outline-light me-2" href= "manage_products.php">
+
+               Products
+
+           </a>
+
+           <a class= "btn btn-outline-light me-2" href= "manage_orders.php">
+
+               Orders
+
+           </a>
+
+           <a class= "btn btn-danger me-2" href= "../logout_system.php">
+
+               Logout
+
+           </a>
+
+            
+        </div>
+
+
+   </div>  
+
+</nav>
